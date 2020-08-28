77f278376d26ed72d871e99ecc34e4e53ebe6721
hiddehs <hidde.schultze@gmail.com>
2020-08-28 22:40:04 +0200

    Add stats UI

--- components/slides/stat.js
+++ components/slides/stat.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-export default function Stat ({ stats }) {
+export default function Stat ({ server }) {
   return <div
     className="stat bg-gray-300 shadow-md py-4 px-6 inline-flex flex-wrap items-center">
     <div
       className="stat-circle bg-orange-500 rounded-full text-white flex items-center justify-center">
-      <h5>50%</h5>
+      <h5>{server.stats.cpu*100}%</h5>
     </div>
     <div className="stat-details ml-6 leading-tight">
       <h4>
         cpu @<br/>
-        web1.drimpy.com
+        {server.name}
       </h4>
     </div>
     <style jsx>{`
--- components/slides/stats.js
+++ components/slides/stats.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Stat from './stat'
+import useSWR from 'swr'
 
 export default function Stats(){
 
+  const fetcher = (args) => fetch(args).then(res => res.json())
+  const {data, error} = useSWR("/api/stats", fetcher);
+
+  if(!data) return 'loading';
+  console.log(data.servers)
+
   return <>
     <div className="stats h-full flex items-center">
-     <Stat/>
-     <Stat/>
-     <Stat/>
-     <Stat/>
+      {data.servers.map((server)=>{
+        return <Stat server={ server}/>
+      })}
+     {/*<Stat/>*/}
+     {/*<Stat/>*/}
+     {/*<Stat/>*/}
     </div>
   </>
 }
